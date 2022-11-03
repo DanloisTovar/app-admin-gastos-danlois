1503701f47400e0f8c9a8ba54c671b46e6989e36
Danlois Tovar <danlois.tovar@gmail.com>
2022-11-02 22:06:35 -0300

    init Proyect

---
+++ src/js/App.jsx
@@ -0,0 +1,14 @@
+// ! import librerias:
+
+// !import estilos:
+import '../styles/App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Proyecto de de administracion de gastos</h1>
+        </div>
+    );
+}
+
+export default App;
